4d2e76bea88302986695d9c260825df5d5775903
zxx-0717 <1024988917@qq.com>
2025-03-06 17:53:23 +0800

    更改odom数据的话题名icp_odom

--- capella_ros_dock/src/manual_dock.cpp
+++ capella_ros_dock/src/manual_dock.cpp
@@ -117,7 +117,7 @@ namespace capella_ros_dock
                 );
 
                 odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-                        "odom_plicp",
+                        "icp_odom",
                         1,
                         std::bind(&ManualDock::odom_sub_callback, this, std::placeholders::_1),
                         sub_ops9
